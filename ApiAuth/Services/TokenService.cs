f910095cde58f11685f7eab8b65bc6fb4d26182d
Carlos Eduardo <carlos.pita@fitbank.com.br>
2023-01-31 14:02:57 -0300

    Settings e SecurityHandler construídos.

---
+++ ApiAuth/Services/TokenService.cs
@@ -0,0 +1,28 @@
+using ApiAuth.Models;
+using System;
+using System.Text;
+using System.Security.Claims;
+
+namespace ApiAuth.Services
+{
+    public static class TokenService
+    {
+        public static string GenerateToken(User user)
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(Settings.Secret);
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new[]
+                {
+                    new Claim(ClaimType.Name, user.Username), 
+                    new Claim(ClaimType.Role, user.Role) 
+                }),
+                Expires = DateTime.UtcNow.AddHours(8),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            return tokenHandler.WriteToken(token);
+        }
+    }
+}
---
+++ ApiAuth/Settings.cs
@@ -0,0 +1,7 @@
+namespace ApiAuth
+{
+    public static class Settings
+    {
+        public static string Secret = "arvh6te8aqevq6aeh86ahvr5vqa6";
+    }
+}
